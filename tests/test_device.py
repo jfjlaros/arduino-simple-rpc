ac64ff0442f17eb2a00d37674ae7bc0d58da5e0a
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2019-04-07 14:04:29 +0200

    Made live device testing optional.

--- tests/test_device.py
+++ tests/test_device.py
@@ -1,10 +1,17 @@
+from os.path import exists
+
+from pytest import mark
+
 from simple_rpc import Interface
 from simple_rpc.simple_rpc import _version
 
 
-_interface = Interface('/dev/ttyACM0', autoconnect=False)
+_device = '/dev/ttyACM0'
+if exists(_device):
+    _interface = Interface(_device, autoconnect=False)
 
 
+@mark.skipif(not exists(_device), reason='device not connected')
 class TestLib(object):
     def test_pre_open(self):
         assert not _interface.is_open()
@@ -16,47 +23,30 @@ class TestLib(object):
         assert _interface.methods != {}
 
     def test_version(self):
-        assert _interface.version() == _version
+        assert _interface._version == _version
 
     def test_ping(self):
         assert _interface.ping(3) == 3
 
     def test_type_1(self):
-        assert _interface.methods['version']['return']['typename'] == 'str'
-
-    def test_type_2(self):
         assert _interface.methods['ping']['return']['typename'] == 'int'
 
     def test_fmt_1(self):
-        assert _interface.methods['version']['return']['fmt'] == 's'
-
-    def test_fmt_2(self):
-        assert _interface.methods['ping']['return']['fmt'] == 'B'
+        assert _interface.methods['ping']['return']['fmt'] == b'B'
 
     def test_param_1(self):
-        assert _interface.methods['version']['parameters'] == []
-
-    def test_param_2(self):
         assert _interface.methods['ping']['parameters'][0]['typename'] == 'int'
 
-    def test_param_3(self):
-        assert _interface.methods['ping']['parameters'][0]['fmt'] == 'B'
+    def test_param_2(self):
+        assert _interface.methods['ping']['parameters'][0]['fmt'] == b'B'
 
-    def test_param_4(self):
+    def test_param_3(self):
         assert _interface.methods['ping']['parameters'][0]['name'] == 'data'
 
     def test_doc_1(self):
-        assert _interface.methods['version']['doc'] == 'Protocol version.'
-
-    def test_doc_2(self):
-        assert (
-            _interface.methods['version']['return']['doc'] ==
-            'Version string.')
-
-    def test_doc_3(self):
         assert _interface.methods['ping']['doc'] == 'Echo a value.'
 
-    def test_doc_4(self):
+    def test_doc_2(self):
         assert _interface.methods['ping']['parameters'][0]['doc'] == 'Value.'
 
     def test_close(self):
