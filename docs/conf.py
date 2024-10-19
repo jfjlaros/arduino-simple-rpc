b7900ddb9e8f757919b653a6a294220b28eda961
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2024-10-19 20:58:28 +0200

    ReadTheDocs theme.

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,7 @@ from importlib.metadata import metadata
 
 _package_metadata = metadata('arduino_simple_rpc')
 
+html_theme = 'sphinx_rtd_theme'
 author = _package_metadata['Author']
 copyright = '2019, {}'.format(author)
 project = _package_metadata['Name']
